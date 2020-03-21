5af2bd52e0e1a0a95c660b99e7817353c6a1511e
Rhytz <mail@rhytz.rocks>
2020-03-21 22:36:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# rhytz-gtac" 
---
+++ rhytz-minigames/minigames_s.js
@@ -0,0 +1,19 @@
+const MINIGAME_INTERVAL			= 20;
+const PREPARATION_TIME			= 3;
+const MGS_VIRTUALWORLD			= 0;
+const SCORE_SOUND				= 147
+const FINISHED_SOUND			= 0;
+
+const minigameState = {
+	"Countdown":1,
+	"Started":2,
+	"Ended":3
+}
+
+bindEventHandler("OnResourceStart", thisResource, function(event, resource) {
+
+});
+
+function prepareRandomMinigame(){
+	
+}
